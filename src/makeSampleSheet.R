57109d9d86f14bf662b368d9c7559736fd0a36cf
Stephanie Hickey <hickeys6@msu.edu>
2026-08-14 12:47:27 -0400

    update

--- src/makeSampleSheet.R
+++ src/makeSampleSheet.R
@@ -1,49 +1,38 @@
 # make sample sheet for nfcore-rna-seq
 library(tidyverse)
-library(readxl)
 
-meta = read_excel("/mnt/research/bioinformaticsCore/projects/FowkesGajan/BCC276_FowkesGajan_RNAseq/data/Comparisons for RNA Seq 7-2026.xlsx",
-                  range = "A1:C25")
+project_dir = "/mnt/research/bioinformaticsCore/projects/chong_davidson/BCC125_sea_lamprey_scRNA"
+meta = read.csv(paste0(project_dir, "/data/SraRunTable.csv"))
+meta_sample = 
+  meta %>% 
+  select(Run, Sample.Name, sex) %>%
+  mutate(Run.Sample = paste0(Run, "_", Sample.Name))
 
-meta$Replicate = rep(c(1:3), 8)
-meta$Timepoint = gsub(" ", "", meta$Timepoint)
-
-meta = 
-  meta %>%
-  mutate(SampleName = paste0("Tx.", `Tx`, "_", Timepoint, "_rep", Replicate),
-         Sample = as.character(Sample),
-         SampleGroup = paste0("Tx.", `Tx`, "_", Timepoint))
-
-write.table(meta, file = "/mnt/research/bioinformaticsCore/projects/FowkesGajan/BCC276_FowkesGajan_RNAseq/data/meta.csv", 
-            sep = ",", 
-            quote=F,
-            row.names = F)
-
-data_dir1 = "/mnt/research/bioinformaticsCore/projects/FowkesGajan/BCC276_FowkesGajan_RNAseq/data/fastq/20260720_mRNASeq_PE150"
+data_dir1 = "/mnt/scratch/hickeys6/yasmin_et_at_PRJNA749754/fastq"
 
 R1 = list.files(data_dir1, 
-                pattern = "_R1.fastq.gz", 
+                pattern = "_1.fastq", 
                 full.names = TRUE)
 
 R2 = list.files(data_dir1, 
-                pattern = "_R2.fastq.gz", 
+                pattern = "_2.fastq", 
                 full.names = TRUE)
 
-sample = gsub("_R1.fastq.gz", "", basename(R1))
+run = gsub("_1.fastq", "", basename(R1))
   
-samp1 = data.frame(Sample = sample,
+samp1 = data.frame(Run = run,
                   fastq_1 = R1,
                   fastq_2 = R2,
                   strandedness = "auto") %>%
-  left_join(meta)
+  left_join(meta_sample)
 
 
-samp_final = data.frame(sample = samp1$SampleName,
+samp_final = data.frame(sample = samp1$Run.Sample,
                         fastq_1 = samp1$fastq_1,
                         fastq_2 = samp1$fastq_2,
                         strandedness = "auto")
 
-write.table(samp_final, file = "/mnt/research/bioinformaticsCore/projects/FowkesGajan/BCC276_FowkesGajan_RNAseq/data/sample_sheet.csv", 
+write.table(samp_final, file = paste0(project_dir, "/data/sample_sheet.csv"), 
             sep = ",", 
             quote=F,
             row.names = F)
